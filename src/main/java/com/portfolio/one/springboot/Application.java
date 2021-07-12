88b496071b30ccef87bd9d5b5a8c4e14944237f2
eilin32 <dkdlfosem22@naver.com>
2021-07-12 13:57:06 +0900

    테스트오류처리완료

--- src/main/java/com/portfolio/one/springboot/Application.java
+++ src/main/java/com/portfolio/one/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing// JPA Auditing 활성화
+//@EnableJpaAuditing// JPA Auditing 활성화 -> 원활한 test를 위해 @SpringBootApplication과 분리함, config/JpaConfig로 이동
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
